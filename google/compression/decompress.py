e81a088d350748d9049bfc855427c6f53402e995
va@no.where.com <va@no.where.com>
2019-02-24 21:25:41 +0200

    fix

--- google/compression/decompress.py
+++ google/compression/decompress.py
@@ -1,8 +1,14 @@
 import sys
 
 def find_closing_bracket(str):
+	"""
+	input: "[[string]]"
+	output: 9 (i.e. the closing bracket of the bracket at pos. 0 is at index 9)
+	"""
+	if str[0] != "[":
+		return None
 	depth = 0
-	for i in range(0, len(str)):
+	for i in range(1, len(str)):
 		if str[i] == "[":
 			depth += 1
 		if str[i] == "]":
@@ -11,13 +17,17 @@ def find_closing_bracket(str):
 			return i
 
 def get_num(str, start):
+	"""
+	input: "100[ala]", 0  (i.e. get number starting off at index 0)
+	output: 100, 3 (i.e. number is 100 and token starts off at index 3)
+	"""
 	numstr = ""
 	for i in range(start, len(str)):
 		if str[i].isdigit():
 			numstr += str[i]
 		else:
 			break
-	return int(numstr), i+1
+	return int(numstr), i
 
 def inflate(str):
 	inflated = ""
