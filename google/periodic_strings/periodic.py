cdf0f82c11a59943d5215e933919913149a8b961
vas <lou@kou.mi>
2019-03-02 00:03:27 +0200

    2nd algo

--- google/periodic_strings/periodic.py
+++ google/periodic_strings/periodic.py
@@ -1,7 +1,8 @@
 # Find whether string S is periodic
 import logging
+import time
 
-def period(s):
+def period_1(s):
     # an odd length string cannot be periodic
     if len(s)%2 != 0:
         return None
@@ -19,10 +20,40 @@ def period(s):
     return None
 
 
+def period_2(s):
+    # an odd length string cannot be periodic
+    if len(s)%2 != 0:
+        return None
+    buffer = ""
+    per = "" 
+    p = 0
+    for i in range(0, len(s)):
+        buffer += s[i]
+        if i > 0 and s[i] == per[p]:
+            p += 1
+            if p == len(per):
+                p = 0
+        else:
+            p = 0
+            per += buffer
+            buffer = "" 
+    print(per)
+    if len(per) < len(s):
+        return per
+    else:
+        return None
+           
+
 if __name__ == "__main__":
-    assert(period("abababab") == "ab")
-    assert(period("abcdabc") == None)
-    assert(period("aaabaaa") == None)
+    assert(period_2("abababab") == "ab")
+    assert(period_2("abcdabc") == None)
+    assert(period_2("aaabaaa") == None)
     s = "".join(["reallyreallylongperiodicstring" * 1000000])
-    assert(period(s) == "reallyreallylongperiodicstring")
+    t1 = time.time()
+    assert(period_2(s) == "reallyreallylongperiodicstring")
+    t2 = time.time()
+    t3 = time.time()
+    assert(period_1(s) == "reallyreallylongperiodicstring")
+    t4 = time.time()
+    print("%s, %s" % (t2-t1, t4-t3))
 
